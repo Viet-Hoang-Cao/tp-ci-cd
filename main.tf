89b481228ea1f9ed466c42462978694f58b49faa
Ubuntu <azureuser@worker-tp-azure.fkpuu1ds5ieezie21mbcfosarh.parx.internal.cloudapp.net>
2025-10-17 16:24:39 +0000

    ajout des nouveaux fichiers

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name = "tfstate-rg"
+        storage_account_name = "tfstateabcd1234"
+        container_name = "tfstate"
+        key = "rg-demo/terraform.tfstate"
+    }
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.100"
+        }
+    }
+}
+provider "azurerm" {
+    features {}
+}
+resource "azurerm_resource_group" "rg" {
+    name = var.resource_group_name
+    location = var.location
+}
